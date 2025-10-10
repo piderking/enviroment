acbabf92b5034b932703865780be1e8b77d0b88a
piderking <piderking8@gmail.com>
2025-10-10 07:19:42 -0700

    auto creating directories and inital csv

--- src/python/config.py
+++ src/python/config.py
@@ -2,21 +2,49 @@ import json
 from typing import List, Any, Optional, Union, Dict
 import logging
 from colorlog import ColoredFormatter
-
+import os
 
 file = open("../config.json", "r")
 CONFIG: Dict[str, Any] = json.loads(file.read())
 file.close()
 
 
+def config_validate():
+    for v in CONFIG.values():
+        if type(v) is dict:
+            val = v.get("value")
+            if v.get("type") is None:
+                logger.warning("[CONFIG] Always require a type")
+                continue
+            if v.get("type") == "directory":
+                if not os.path.exists(os.path.abspath(val)):
+                    logger.info(f"Creating Directory {os.path.abspath(val)}")
+                    os.makedirs(os.path.abspath(val))
+
+
+def get_config(key: str) -> Any:
+
+    v = CONFIG.get(key)
+    if type(v) is dict:
+        if v["type"] == "directory":
+            v = os.path.abspath(v["value"])
+        else:
+            v = v["value"]
+    return v
+
+
 def update_config(key: str, value: Any) -> Any:
     v = CONFIG.get(key)
 
-    CONFIG[key] = value
+    if type(v) is dict:
+        CONFIG[key]["value"] = value
+    else:
+        CONFIG[key] = value
 
     with open("../config.json", "w") as f:
         f.write(json.dumps(CONFIG))
 
+    config_validate()
     return v
 
 
@@ -49,4 +77,4 @@ handler.setFormatter(formatter)
 # Add the handler to the logger
 logger.addHandler(handler)
 
-# logger.error(CONFIG)
+config_validate()
--- src/python/db.py
+++ src/python/db.py
@@ -1,24 +1,68 @@
-import sqlite3
-import json
+from config import logger, get_config
+from typing import List, Optional
+import os
+CSV_PATH = get_config('csv')
 
-from config import CONFIG
 
-db = sqlite3.connect(CONFIG['db'])
+type Item = (str, str)  # type: ignore
+headers = ["id", "content"]
+item_types = [str, str]
+# id, content --> string
 
-cursor = db.cursor()
 
-# Create a table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        age INTEGER
-    )
-''')
+class MemoryBank():
 
-# Insert data
-cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Alice', 30))
-cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Bob', 25))
+    data: (str, str)
 
-# Commit changes
-db.commit()
+    @classmethod
+    def verify_type_signature(cls, item: Item) -> bool:
+
+        if not len(item_types) == len(item):
+            return False
+
+        return all([type(tm) is tp for tp, tm in zip(item_types, item)])
+
+    @classmethod
+    def data_from_csv(cls, fileName: str) -> List[Item]:
+        data_path = os.path.join(get_config("csv"), f"{fileName}.csv")
+        if not os.path.exists(data_path):
+            logger.warn(f"Data File for reading: {data_path} is empty...")
+            return []
+
+        final = []
+        with open(data_path, "r") as file:
+            delim = get("csv-delimiter")
+            for idx, line in enumerate(file):
+                values = [typ(val) for val, typ in zip(
+                    line.split(delim), item_types)]
+                # Add to Final Value List
+                final.append(values)
+
+        return final
+
+    def write_csv(self, fileName: str, output_full=False) -> Optional[str]:
+        data_path = os.path.join(get_config("csv"), f"{fileName}.csv")
+        if not os.path.exists(data_path):
+            logger.error(f"Data File for writing: {data_path} is empty...")
+            raise FileNotFoundError(
+                f"Data File for writing: {data_path} is empty...")
+
+        with open(data_path, "w") as file:
+            delim = get("csv-delimiter")
+
+            header = delim.join(headers)
+            content = "\n".join(
+                [delim.join([str(item) for item in d]) for d in data])
+
+            file.write(header + "\n" + content)
+
+            if output_full:
+                return header + "\n" + content
+
+            return
+
+    def __init__(self, data: List[Item]):
+        # parse CSV
+
+        self.data = self.data_from_csv("data")
+        self.data.extend([d for d in data if verify_type_signature(d)])
