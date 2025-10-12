f8a4eeb3fb7b2a985d70ca18905db3e6c6b6984d
piderking <piderking8@gmail.com>
2025-10-12 11:42:14 -0700

    working csv database

--- src/python/db.py
+++ src/python/db.py
@@ -1,68 +1,131 @@
 from config import logger, get_config
-from typing import List, Optional
+from typing import List, Optional, Any
 import os
+import pandas as pd
 CSV_PATH = get_config('csv')
 
 
-type Item = (str, str)  # type: ignore
 headers = ["id", "content"]
-item_types = [str, str]
+item_types = [int, str]
 # id, content --> string
 
 
 class MemoryBank():
-
+    """For Content (NOT EMBEDDINGS (INCLUDES STRINGS))"""
+    fileName: str = "data"
+    headers: List[str]
     data: (str, str)
 
     @classmethod
-    def verify_type_signature(cls, item: Item) -> bool:
+    def verify_type_signature(cls, item: List[Any]) -> item_types:
 
         if not len(item_types) == len(item):
             return False
 
-        return all([type(tm) is tp for tp, tm in zip(item_types, item)])
+        try:
 
-    @classmethod
-    def data_from_csv(cls, fileName: str) -> List[Item]:
-        data_path = os.path.join(get_config("csv"), f"{fileName}.csv")
+            return [tp(tm) for tp, tm in zip(item_types, item)]
+        except ValueError as e:
+            logger.error(f"Invalid Types for {e}")
+
+        return []
+
+    def data_from_csv(self) -> List[item_types]:
+        data_path = os.path.join(get_config("csv"), f"{self.fileName}.csv")
+        logger.debug(f"Reading {data_path}")
         if not os.path.exists(data_path):
             logger.warn(f"Data File for reading: {data_path} is empty...")
+            self.headers = headers
             return []
 
         final = []
-        with open(data_path, "r") as file:
-            delim = get("csv-delimiter")
-            for idx, line in enumerate(file):
-                values = [typ(val) for val, typ in zip(
-                    line.split(delim), item_types)]
-                # Add to Final Value List
-                final.append(values)
 
+        # ERROR IS OCCURING IN THIS PART
+        with open(data_path, "r") as file:
+            delim = get_config("csv-delimiter")
+
+            for idx, line in enumerate(file):
+
+                line = line.strip()
+                # logger.debug(f"Line#{str(idx)} == {line}")
+                # line.removesuffix("\n")
+
+                if idx == 0:
+                    self.headers = line.split(delim)
+                    logger.debug(f"Adding Header: {line}")
+                    continue
+                else:
+                    splt = line.split(delim)
+                    logger.debug(f"Adding Line: #{idx} {line} ")
+                    if not len(splt) == len(item_types):
+                        logger.error(
+                            f"Invalid Size: Line: {len(splt) } != Types: {len(item_types)}; Skipping Row")
+                        continue
+                    try:
+                        out = [typ(val) for val, typ in zip(splt, item_types)]
+                        # logger.debug(f"Line Output: {out}")
+                        final.append(out)
+                    except ValueError as e:
+                        logger.error(f"Invalid Types for Line: {idx}. {e}")
+
+                # values = [typ(val) for val, typ in zip(
+                #     line.split(delim), item_types)]
+                # # Add to Final Value List
+
+        logger.debug("Finished Reading File")
         return final
 
-    def write_csv(self, fileName: str, output_full=False) -> Optional[str]:
-        data_path = os.path.join(get_config("csv"), f"{fileName}.csv")
+    def write_csv(self, output_full=False) -> Optional[str]:
+        data_path = os.path.join(get_config("csv"), f"{self.fileName}.csv")
         if not os.path.exists(data_path):
-            logger.error(f"Data File for writing: {data_path} is empty...")
-            raise FileNotFoundError(
-                f"Data File for writing: {data_path} is empty...")
+            logger.debug(f"Data File: writing new file {data_path}...")
 
         with open(data_path, "w") as file:
-            delim = get("csv-delimiter")
-
-            header = delim.join(headers)
+            delim = get_config("csv-delimiter")
+            header = delim.join(self.headers)
             content = "\n".join(
-                [delim.join([str(item) for item in d]) for d in data])
+                [delim.join([str(item) for item in d]) for d in self.data])
 
-            file.write(header + "\n" + content)
-
+            if len(self.data) > 0:
+                file.write(header + "\n" + content)
+            else:
+                file.write(header)
             if output_full:
                 return header + "\n" + content
 
             return
 
-    def __init__(self, data: List[Item]):
+    def __init__(self, data: Optional[List[item_types]] = None):
         # parse CSV
 
-        self.data = self.data_from_csv("data")
-        self.data.extend([d for d in data if verify_type_signature(d)])
+        logger.debug("Creating MemoryBank Object from CSV File")
+        self.data = self.data_from_csv()
+
+        if data is not None:
+
+            logger.debug("Data Given... Validating and Adding")
+            self.data.extend([self.verify_type_signature(d) for d in data])
+
+        # write any additional data to csv
+        self.update(output_full=True)
+
+    def update(self, output_full=False):
+
+        self.write_csv(output_full=output_full)
+
+    def size(self) -> (int, int):
+        return (len(self.data), len(self.data[0]) if len(self.data) > 0 else 0)
+
+    def __repr__(self) -> str:
+        # text = "\n".join(["|".join(row) for row in self.data])
+
+        # return f"Size: {str(self.size())}\n\n|{" | ".join(self.headers)}|\n{self.data}"
+        return ""
+
+
+if __name__ == "__main__":
+
+    db = MemoryBank(data=[["1", "world"]])
+    print(db.headers)
+
+    print(db.data)
